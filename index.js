f5ec3b751f2792d8d524a64124d15b8197db5334
OmarDraz <omarnader6636@gmail.com>
2022-08-19 15:54:47 +0200

    cors

--- index.js
+++ index.js
@@ -14,7 +14,12 @@ const io = require('socket.io')(server);
 //use express static folder
 app.use(express.static('public'))
 
-app.use(cors())
+const corsOptions ={
+    origin:'http://localhost:3000', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
  
 
 app.use(bodyParser.json())
